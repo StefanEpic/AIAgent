49c2dd92fe0fa1d219b6454cdfe8721f5aedb89d
Stefan <118810289+StefanEpic@users.noreply.github.com>
2025-08-24 17:13:18 +0300

    Update schemas.py

--- tools/schemas.py
+++ tools/schemas.py
@@ -1,8 +1,20 @@
+from typing import List
+
 from pydantic import BaseModel, Field
 
 
-class Employee(BaseModel):
-    """Данные о сотруднике."""
-    name: str = Field(description="Фамилия имя Отчество")
+class EmployeeForInfoList(BaseModel):
+    """Данные о сотруднике для Листа ознакомления."""
+    name: str = Field(description="Фамилия Имя Отчество")
     job_title: str = Field(description="Название должности")
     baza: str = Field(description="Топливная база")
+
+
+class EmployeeForOrderB(BaseModel):
+    """Данные о сотруднике для Приказа Б."""
+    surname: str = Field(description="Фамилия")
+    name: str = Field(description="Имя")
+    patronymic: str = Field(description="Отчество")
+    number: str = Field(description="Табельный номер")
+    dates_of_training: List[str] = Field(description="Даты обучения сотрудника")
+    knowledge_test_date: str = Field(description="Дата проверки знаний сотрудника")
